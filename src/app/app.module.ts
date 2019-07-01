50ca7387bc93db848b983d2fdc1df24fd8aa0303
Kevin Jolley <serrowpls@gmail.com>
2019-06-30 22:30:01 -0500

    material UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SecondpageComponent } from './secondpage/secondpage.component';
-import { HomepageComponent } from './homepage/homepage.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { SecondpageComponent } from "./secondpage/secondpage.component";
+import { HomepageComponent } from "./homepage/homepage.component";
+import { NavigationComponent } from "./navigation/navigation.component";
+import { FooterComponent } from "./footer/footer.component";
+import { HeaderComponent } from "./header/header.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+// Material Imports
+
+import { MatButtonModule, MatCheckboxModule } from "@angular/material";
 
 @NgModule({
   declarations: [
@@ -22,9 +26,12 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule
   ],
+  exports: [MatButtonModule, MatCheckboxModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
