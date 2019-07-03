fa77d6213a80d867b3451401494cfcc15c3ee87e
Kevin Jolley <serrowpls@gmail.com>
2019-07-03 03:07:13 -0500

    added material ui -- again

---
+++ src/app/material.component.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+// Material imports
+
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+  imports: [MatButtonModule],
+  exports: [MatButtonModule],
+})
+export class MaterialModule {}
